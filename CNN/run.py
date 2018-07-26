6b8ff63557a90e68db7cf23aa0e317d5d538e299
Ryan C <ryan@ryancross.xyz>
2018-07-26 23:13:56 +0100

    Clean up from merge.

--- CNN/run.py
+++ CNN/run.py
@@ -127,11 +127,6 @@ def main():
     if FLAGS.training:
         check_existing_folder(FULL_CHECKPOINT_PATH)
         check_existing_folder(FULL_SUMMARY_PATH)
-        
-    if FLAGS.save_replays_every > 0:
-        if FLAGS.replay_dir == "":
-            print("Need to specify a replay dir!")
-            return
 
     if FLAGS.save_replays_every > 0:
         if FLAGS.replay_dir == "":
